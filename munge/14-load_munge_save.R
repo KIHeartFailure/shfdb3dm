7adb272126d1873ab5aa1fc9c2a242f9bd81e207
Lina Benson <lina.benson@ki.se>
2020-05-26 14:19:50 +0200

    add v no for stata export

--- munge/14-load_munge_save.R
+++ munge/14-load_munge_save.R
@@ -12,8 +12,10 @@ ProjectTemplate::reload.project(
 
 # For webb only
 
-save(file = "./data/rsdata_for_webb.RData", 
-     list = c("rsdata", "flow", "ncontrols", "outcommeta", "ccimeta", "deathmeta"))
+save(
+  file = "./data/rsdata_for_webb.RData",
+  list = c("rsdata", "flow", "ncontrols", "outcommeta", "ccimeta", "deathmeta")
+)
 
 # Version number
 
@@ -26,23 +28,33 @@ dir.create(paste0("./data/v", version))
 
 # RData
 
-save(file = paste0("./data/v", version, "/rsdata", version, ".RData"), 
-     list = c(paste0("rsdata", version)))
-save(file = paste0("./data/v", version, "/rsdatanum", version, ".RData"), 
-     list = c(paste0("rsdatanum", version)))
+save(
+  file = paste0("./data/v", version, "/rsdata", version, ".RData"),
+  list = c(paste0("rsdata", version))
+)
+save(
+  file = paste0("./data/v", version, "/rsdatanum", version, ".RData"),
+  list = c(paste0("rsdatanum", version))
+)
 
 # Txt
 
-write.table(rsdata, 
-            file = paste0("./data/v", version, "/rsdata", version, ".txt"), 
-            quote = FALSE, sep = "\t", row.names = FALSE)
-write.table(rsdatanum, 
-            file = paste0("./data/v", version, "/rsdatanum", version, ".txt"),
-            quote = FALSE, sep = "\t", row.names = FALSE)
+write.table(rsdata,
+  file = paste0("./data/v", version, "/rsdata", version, ".txt"),
+  quote = FALSE, sep = "\t", row.names = FALSE
+)
+write.table(rsdatanum,
+  file = paste0("./data/v", version, "/rsdatanum", version, ".txt"),
+  quote = FALSE, sep = "\t", row.names = FALSE
+)
 
-# Stata 13
+# Stata 14
 
-write_dta(rsdata, 
-           path = paste0("./data/v", version, "/rsdata", version, ".dta"))
-write_dta(rsdatanum, 
-           path = paste0("./data/v", version, "/rsdatanum", version, ".dta"))
+write_dta(rsdata,
+  path = paste0("./data/v", version, "/rsdata", version, ".dta"),
+  version = 14
+)
+write_dta(rsdatanum,
+  path = paste0("./data/v", version, "/rsdatanum", version, ".dta"),
+  version = 14
+)
